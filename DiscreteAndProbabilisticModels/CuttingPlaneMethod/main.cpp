30eca80964bd8788b050663cde6426a1deaa9c1e
Ihtier <ihtier_@outlook.com>
2022-12-18 19:04:37 +0300

    delete useless code

--- DiscreteAndProbabilisticModels/CuttingPlaneMethod/main.cpp
+++ DiscreteAndProbabilisticModels/CuttingPlaneMethod/main.cpp
@@ -14,46 +14,6 @@ int main() {
       // 3 * x1 + 2 * x2 + x3 - 3 * x4  = 18
       //   - x1 + 3 * x2 +  0 + 4 * x4 <= 24
 
-      //
-
-      // 2 * x1 + 3 * x2 + 0 * x3 - x4 -> max
-
-      // 2 * x1     - x2 +  0 - 2 * x4 + x5 +  0 = 16
-      // 3 * x1 + 2 * x2 + x3 - 3 * x4 +  0 +  0 = 18
-      //   - x1 + 3 * x2 +  0 + 4 * x4 +  0 + x6 = 24
-
-      CanonicalContext context;
-      context.objectiveFunctionCoefficients = {2.0f,  3.0f, 0.0f,
-                                               -1.0f, 0.0f, 0.0f};
-
-      // clang-format off
-      context.constraintsCoefficients = {
-          { 2.0f, -1.0f,  0.0f, -2.0f, 1.0f, 0.0f},
-          { 3.0f,  2.0f,  1.0f, -3.0f, 0.0f, 0.0f},
-          {-1.0f,  3.0f,  0.0f,  4.0f, 0.0f, 1.0f}
-      };
-      // clang-format on
-
-      context.constraintsConstants = {16.0f, 18.0f, 24.0f};
-
-      CanonicalSolverUniquePtr simplex = SimplexAlgorithm::create(context);
-
-      if (!simplex) {
-        return -1;
-      }
-
-      VectorCoefficients answer = simplex->calculate(printCallback);
-
-      std::cout << "answer: " << answer << std::endl;
-    }
-
-    {
-      // 2 * x1 + 3 * x2 + 0 * x3 - x4 -> max
-
-      // 2 * x1     - x2 +  0 - 2 * x4 <= 16
-      // 3 * x1 + 2 * x2 + x3 - 3 * x4  = 18
-      //   - x1 + 3 * x2 +  0 + 4 * x4 <= 24
-
       SolveContext context;
       context.objectiveFunctionCoefficients = {2.0f, 3.0f, 0.0f, -1.0f};
       context.type = OptimizationType::Max;

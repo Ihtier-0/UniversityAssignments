b61708879a2aaf486a3632740c0e6bb021949eb0
Ihtier <ihtier_@outlook.com>
2022-12-27 00:17:05 +0300

    delete useless code

--- DiscreteAndProbabilisticModels/CuttingPlaneMethod/main.cpp
+++ DiscreteAndProbabilisticModels/CuttingPlaneMethod/main.cpp
@@ -8,49 +8,6 @@
 
 int main() {
   try {
-    // // SimplexAlgorithm
-    // {
-    //   // 2 * x1 + 3 * x2 + 0 * x3 - x4 -> max
-
-    //   // 2 * x1     - x2 +  0 - 2 * x4 <= 16
-    //   // 3 * x1 + 2 * x2 + x3 - 3 * x4  = 18
-    //   //   - x1 + 3 * x2 +  0 + 4 * x4 <= 24
-
-    //   SolveContext context;
-    //   context.objectiveFunctionCoefficients = {
-    //       2.0f,
-    //       3.0f,
-    //       0.0f,
-    //       -1.0f,
-    //   };
-    //   context.type = OptimizationType::Max;
-
-    //   context.constraints = {
-    //       {{2.0f, -1.0f, 0.0f, -2.0f}, Sign::le, 16},
-    //       {{3.0f, 2.0f, 1.0f, -3.0f}, Sign::eq, 18},
-    //       {{-1.0f, 3.0f, 0.0f, 4.0f}, Sign::le, 24},
-    //   };
-
-    //   CanonicalAdapterUniquePtr simplex =
-    //       CanonicalAdapter::create(context, SimplexAlgorithm::create);
-
-    //   if (!simplex) {
-    //     return EXIT_FAILURE;
-    //   }
-
-    //   std::pair<CanonicalSolver::EndType, VectorCoefficients> answer =
-    //       simplex->calculate(printCallback);
-
-    //   if (answer.first == CanonicalSolver::End) {
-    //     std::cout << "answer: " << answer.second << std::endl;
-    //   } else {
-    //     std::cout << "answer: " << CanonicalSolver::toString(answer.first)
-    //               << std::endl;
-    //   }
-    // }
-
-    // std::cout << std::endl;
-
     // CuttingPlaneMethod
     {
       // 2 * x1 + 4 * x2 - 8 * x3 -> max

e5cbc7e9b039b5b308811986a2c41fe4153a72ee
Ihtier <ihtier_@outlook.com>
2023-01-02 18:04:56 +0300

    fix fractpart

--- DiscreteAndProbabilisticModels/CuttingPlaneMethod/fractpart.cpp
+++ DiscreteAndProbabilisticModels/CuttingPlaneMethod/fractpart.cpp
@@ -2,10 +2,4 @@
 
 #include <cmath>
 
-#include "fuzzycompare.h"
-
-Real fractpart(const Real &real) {
-  Real fractpart, intpart;
-  fractpart = std::modf(real, &intpart);
-  return real > 0.0f || fuzzyEquals(real, 0.0f) ? fractpart : 1.0f + fractpart;
-}
+Real fractpart(const Real &real) { return real - std::floor(real); }
